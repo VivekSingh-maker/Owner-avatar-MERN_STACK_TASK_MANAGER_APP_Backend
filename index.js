769c8695ab2d6b3c5fbeca6b38f0d9baf6384c3e
Vivek Singh <69762818+VivekSingh-maker@users.noreply.github.com>
2024-09-12 21:47:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://velvety-dragon-0569de.netlify.app/"],
+    origin: ["https://velvety-dragon-0569de.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
