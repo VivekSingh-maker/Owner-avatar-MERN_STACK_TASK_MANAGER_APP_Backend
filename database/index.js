16a3b47e822a365b4ec7709390b8bebd9413143b
Vivek Singh <69762818+VivekSingh-maker@users.noreply.github.com>
2024-09-12 20:41:50 +0530

    first commit

---
+++ database/index.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoUrl = process.env.MONGODB_URL;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("MongoDB Connection successfull"))
+  .catch((error) => console.log(`Error occured: ${error}`));
